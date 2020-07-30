b3b67642c1e5cc298448979dc7083f6b4f579f77
litaiping_jp@lakala.com <litaiping_jp@lakala.com>
2020-07-30 16:56:17 +0800

    first commit

---
+++ ltp-spring-redis/src/test/java/com/ltp/spring/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.spring.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
